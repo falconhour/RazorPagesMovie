9a0459a399c1bc54dec663140d83578fb7a8802a
Saquing, Kenneth A <kenneth.a.saquing@accenture.com>
2018-11-13 11:44:22 +0800

    test

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace RazorPagesMovie.Pages
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "Your application description page";
         }
     }
 }
